319f5ad46bb2e07bf100fc9c9de45fbc22f8a386
DmitriySavo <dimitrysavo@gmail.com>
2025-05-18 04:24:27 +0300

    -- Add function to getting all type from database but not from enum class in program

--- src/main/kotlin/daos/StorageItemsTypesTable.kt
+++ src/main/kotlin/daos/StorageItemsTypesTable.kt
@@ -1,19 +1,38 @@
 package com.example.daos
 
+import jakarta.mail.Store
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object StorageItemsTypesTable: Table("storageitemtypes") {
     val typeId = integer("typeid").autoIncrement()
     val typeName = varchar("typename", 50)
     override val primaryKey = PrimaryKey(typeId)
-}
 
-object StorageItemsNames {
-    const val MD = "md"
-    const val FOLDER = "folder"
+    data class StorageType(
+        val id: Int,
+        val mimeType: String
+    )
+
+    fun getStorageTypes() : List<StorageType>? {
+        return try {
+            transaction {
+                StorageItemsTypesTable
+                    .selectAll()
+                    .map { row ->
+                        StorageType(row[typeId], row[typeName])
+                    }
+            }
+        } catch (ex: Exception) {
+            println("Get an exception in getStorageTypes : $ex")
+            null
+        }
+    }
 }
 
 enum class StorageItemsIds(val id: Int, val mimeType: String) {
     md(1, "text/markdown"),
-    folder(2, "")
+    folder(2, ""),
+    png(3, "")
 }
